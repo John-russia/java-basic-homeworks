487a5efff65a376732c4756879dbd24f0accb186
Evgeniy Ivanov <john-ik@yandex.ru>
2024-02-19 23:10:35 +0300

    Homework6 Done!

--- src/main/java/ru/ivanov/java/basic/homeworks/Application.java
+++ src/main/java/ru/ivanov/java/basic/homeworks/Application.java
@@ -1,4 +1,34 @@
 package ru.ivanov.java.basic.homeworks;
 
 public class Application {
+    public static void main(String[] args) {
+        Plate plate = new Plate(120);
+        plate.info();
+        Cat[] cats = {
+                new Cat("Персик", 30),
+                new Cat("Барсик", 40),
+                new Cat("Пупсик", 60),
+                new Cat("Шнапсик", 55),
+                new Cat("Дымок", 15),
+        };
+
+        for (Cat cat : cats) {
+            cat.eat(plate);
+        }
+        for (Cat cat : cats) {
+            cat.info();
+        }
+        plate.info();
+
+        plate.addFoodToPlate(1000);
+        plate.info();
+
+        for (Cat cat : cats) {
+            cat.eat(plate);
+        }
+        for (Cat cat : cats) {
+            cat.info();
+        }
+        plate.info();
+    }
 }
