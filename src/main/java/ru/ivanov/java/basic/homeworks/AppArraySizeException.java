cecf1cd3399d673c08381d43ffb6e4afe734d3d1
Evgeniy Ivanov <john-ik@yandex.ru>
2024-02-26 19:32:32 +0300

    Homework8 Done!

---
+++ src/main/java/ru/ivanov/java/basic/homeworks/AppArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.ivanov.java.basic.homeworks;
+
+public class AppArraySizeException extends Exception {
+    public AppArraySizeException(String message) {
+        super(message);
+    }
+}
